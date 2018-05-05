985b0f0418f6b747a559b2e785b179ee0b6060eb
toshi1127 <toshi.matsumoto.2n@stu.hosei.ac.jp>
2018-05-06 03:15:20 +0900

    Study Redux

---
+++ index.js
@@ -0,0 +1,40 @@
+import { createStore } from 'redux';
+
+//Reducer(State,Stateを変更するメソッド)
+//State
+const initialState = {
+    task: []
+}
+
+//Stateを変更するメソッド
+function tasksReducer(state = initialState, action) {
+    switch (action.type) {
+        case 'ADD_TASK':
+            return {
+                ...state,
+                task: state.task.concat([action.task])
+            };
+        default:
+            return state;
+    }
+}
+
+//Store アプリケーションと一対一の関係。全てのStateを管理している。
+const store = createStore(tasksReducer);
+
+//subscribeを使うと、Stateが変更するたびにコールバックメソッドとして、引数の関数を実行できる。
+function handleChange() {
+    console.log(store.getState())
+}
+const unsubscribe = store.subscribe(handleChange)
+
+//Action
+const addTask = (task) => ({
+    type: 'ADD_TASK',
+    payload: {
+        task
+    }
+});
+
+//Actionの発行。
+store.dispatch(addTask('Storeを学ぶ'));
