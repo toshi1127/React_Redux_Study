88c30642bb10e7712b0faf80aadb3e71dcc1c39a
toshi1127 <toshi.matsumoto.2n@stu.hosei.ac.jp>
2018-05-06 20:36:55 +0900

    Setting:compile react

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                loader: 'babel-loader',
+                test: /\.js$/,
+                options: {
+                    presets: ['react']
+                }
+            }
+        ]
+    }
+};
