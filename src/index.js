680e91d38429471c5f01d1c920e2de229a5dc512
toshi1127 <toshi.matsumoto.2n@stu.hosei.ac.jp>
2018-05-06 21:34:00 +0900

    Add:functions

--- src/index.js
+++ src/index.js
@@ -1,26 +1,46 @@
-import { createStore } from 'redux';
+import { createStore } from 'redux'
 
 //Reducer(State,Stateを変更するメソッド)
 //State
 const initialState = {
+    task: '',
     task: []
 }
 
 //Stateを変更するメソッド
-function tasksReducer(state = initialState, action) {
+//taskを加える
+function taskReducer(state = initialState, action) {
     switch (action.type) {
+        case 'INPUT_TASK':
+            return {
+                ...state,
+                task: action.payload.task
+            }
         case 'ADD_TASK':
             return {
                 ...state,
                 task: state.task.concat([action.payload.task])
-            };
+            }
         default:
-            return state;
+            return state
+    }
+}
+
+//taskをリセットする
+function resetReducer(state = initialState, action) {
+    switch (action.type) {
+        case 'REAET_TASK':
+            return {
+                ...state,
+                task: []
+            }
+        default:
+            return state
     }
 }
 
 //Store アプリケーションと一対一の関係。全てのStateを管理している。
-const store = createStore(tasksReducer);
+const store = createStore(taskReducer)
 
 //subscribeを使うと、Stateが変更するたびにコールバックメソッドとして、引数の関数を実行できる。
 function handleChange() {
@@ -34,8 +54,22 @@ const addTask = (task) => ({
     payload: {
         task: task
     }
-});
+})
+
+const resetTask = (task) => ({
+    type: 'REAET_TASK'
+})
+
+const inputTask = (task) => ({
+    type: 'INPUT_TASK',
+    payload: {
+        task: task
+    }
+})
 
 console.log(store.getState())
 //Actionの発行。
-store.dispatch(addTask('Storeを学ぶ'));
+store.dispatch(addTask('Storeを学ぶ'))
+store.replaceReducer(resetReducer)
+store.dispatch(resetTask())
+console.log(store.getState())
