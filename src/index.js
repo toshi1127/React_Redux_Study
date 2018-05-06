4bb724c403775e12fced436a2b9cf0a956841217
toshi1127 <toshi.matsumoto.2n@stu.hosei.ac.jp>
2018-05-06 23:11:42 +0900

    Add:containers

--- src/index.js
+++ src/index.js
@@ -1,20 +1,23 @@
 import { createStore } from 'redux'
 import { render } from 'react-dom'
 import React from 'react'
-import TodoApp from './components/TodoApp';
+import { Provider } from 'react-redux'
 import configureStore from './store/configerStore';
+import TodoApp from './containers/TodoApp';
 
 const store = configureStore()
 
 //Conponetの描画
-function renderApp(store){
+function renderApp(store) {
     render(
-        <TodoApp store={store}/>,
+        <Provider store={store}>
+            <TodoApp />
+        </Provider>,
         document.getElementById('root')
     );
 }
 
 //Stateの変更のたびに描写
-store.subscribe(()=> renderApp(store))
+store.subscribe(() => renderApp(store))
 //初回の描写
 renderApp(store)
