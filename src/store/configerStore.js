80427081b4daed5672dd56c8c79a25e00564d72b
toshi1127 <toshi.matsumoto.2n@stu.hosei.ac.jp>
2018-05-07 22:30:12 +0900

    make better configure Store

--- src/store/configerStore.js
+++ src/store/configerStore.js
@@ -1,7 +1,22 @@
-import { createStore } from 'redux';
+import {
+    createStore as reduxCreateStore,
+    combineReducers,
+    applyMiddleware
+} from 'redux';
+import { routerReducer, routerMiddleware } from 'react-router-redux';
 import taskReducer from '../reducers/tasks';
 
 export default function configureStore() {
-    const store = createStore(taskReducer);
-    return store;
+    return reduxCreateStore(
+        combineReducers({//ルーティングのためのReducerとアプリケーションの(Stateを管理する)Reducerを合成する必要がある。
+            //taskReducerをtaskというkeyに割り当てる
+            task: taskReducer,
+            //ルーティングのためのReducer
+            router: routerReducer,
+        }),
+        applyMiddleware(
+            //react-redux-routerのMiddleware
+            routerMiddleware(history)
+        )
+    )
 }
