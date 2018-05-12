e8ce65c35f1f3be9fb6a0fd63f3c80cd4909d79b
toshi1127 <toshi.matsumoto.2n@stu.hosei.ac.jp>
2018-05-12 21:57:34 +0900

    use material-ui

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -1,24 +1,45 @@
 import React from 'react'
 import Reboot from 'material-ui/Reboot'
 import Button from 'material-ui/Button'
+import AppBar from 'material-ui/AppBar'
+import ToolBar from 'material-ui/ToolBar'
+import Typography from 'material-ui/Typography'
+import Input from 'material-ui/Input'
+import List, { ListItem, ListItemText } from 'material-ui/List';
+import styled from 'styled-components';
+
+const TodoList = styled.div`
+  padding:16px;
+`
 
 //Conponent
 export default function TodoApp({ task, tasks, inputTask, addTask, redirectToError }) {
     return (
         <div>
             <Reboot />
-            <input type='text' onChange={(e) => inputTask(e.target.value)} />
-            <Button raised color='primary' onClick={() => addTask(task)} >add</Button>
-            <ul>
-                {
-                    tasks.map(function (item, i) {
-                        return (
-                            <li key={i}>{item}</li>
-                        )
-                    })
-                }
-            </ul>
-            <button onClick={() => redirectToError()}>エラーページへ</button>
+            <AppBar position='static'>
+                <ToolBar>
+                    <Typography type='title' color='inherit'>
+                        Todo
+            </Typography>
+                </ToolBar>
+            </AppBar>
+            <TodoList>
+                <Input type='text' onChange={(e) => inputTask(e.target.value)} />
+                <Button raised color='primary' onClick={() => addTask(task)} >add</Button>
+                <List>
+                    {
+                        tasks.map(function (item, i) {
+                            return (
+                                <ListItem key={i}>
+                                    <ListItemText primary={`.${item}`} />
+                                </ListItem>
+                            )
+                        })
+                    }
+                </List>
+                <button onClick={() => redirectToError()}>エラーページへ</button>
+            </TodoList>
         </div>
     )
 }
