d97e7de9fad26add5d7ca1c20a756acffb0ce177
toshi1127 <toshi.matsumoto.2n@stu.hosei.ac.jp>
2018-05-12 21:33:36 +0900

    fix:name

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import styled from 'styled-components';
+import { media } from '../utile/Helper'
+
+const Error = styled.h1`
+  ${media.desktop`color: #F00;`}
+  ${media.tablet`color: #0F0;`}
+  ${media.phone`color: #00F;`}
+`
 
 export default function ErrorComponent() {
     return (
         <div>
-            <h1>エラーページ</h1>
+            <Error>エラーページです</Error>
             <Link to='/'>戻る</Link>
         </div>
     );
